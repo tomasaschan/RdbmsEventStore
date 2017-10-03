0712b858af277798e1e2c2f9658ef3eeedf0d2b4
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-03 17:26:05 +0200

    Improve error handling in SimpleEventRegistry construction

---
+++ src/RdbmsEventStore/EventRegistry/EventTypeRegistrationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RdbmsEventStore.EventRegistry
+{
+    public class EventTypeRegistrationException : InvalidOperationException
+    {
+        public EventTypeRegistrationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
