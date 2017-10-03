b67fcbf2e31410ce11ebd2c29fa7e5c7abae6505
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-03 17:37:38 +0200

    Give translating registry some love

--- src/RdbmsEventStore/EventRegistry/TranslatingEventRegistry.cs
+++ src/RdbmsEventStore/EventRegistry/TranslatingEventRegistry.cs
@@ -4,45 +4,55 @@ using System.Linq;
 
 namespace RdbmsEventStore.EventRegistry
 {
-    public class TranslatingEventRegistry : IEventRegistry
+    public class TranslatingEventRegistry : IComposableEventRegistry
     {
         private readonly IReadOnlyDictionary<string, string> _translations;
         private readonly IReadOnlyDictionary<string, string> _inverseTranslations;
-        private readonly IEventRegistry _registry;
+        private readonly IComposableEventRegistry _inner;
         private readonly bool _translateNewEvents;
 
-        public TranslatingEventRegistry(IReadOnlyDictionary<string, string> translations, IEventRegistry registry, bool translateNewEvents = false)
+        public TranslatingEventRegistry(IReadOnlyDictionary<string, string> translations, IComposableEventRegistry inner, bool translateNewEvents = false)
         {
             _translations = translations;
             if (translateNewEvents)
             {
-                _inverseTranslations = translations.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+                try
+                {
+                    _inverseTranslations = translations.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+                }
+                catch (ArgumentException ex)
+                {
+                    throw new EventTypeRegistrationException(
+                        "Invalid translation registration; two or more old names translate to the same new name.\n" +
+                        "If you want to translate both Foo and Bar to Baz, add Foo=>Bar and Bar=>Baz instead of Foo=>Baz and Bar=>Baz.", ex);
+                }
             }
 
-            _registry = registry;
+            _inner = inner;
             _translateNewEvents = translateNewEvents;
         }
 
         public Type TypeFor(string eventType)
-            => _translations.TryGetValue(eventType, out var translated)
-                ? TypeFor(translated)
-                : _registry.TypeFor(eventType);
+            => _inner.TypeFor(TranslationFor(eventType));
 
         public string NameFor(Type eventType)
-            => InverseTranslate(this._registry.NameFor(eventType));
+            => InverseTranslationFor(_inner.NameFor(eventType));
 
-        private string InverseTranslate(string registeredName)
-            => _translateNewEvents && _inverseTranslations.TryGetValue(registeredName, out var translated)
-                ? InverseTranslate(translated)
-                : registeredName;
+        private string TranslationFor(string eventType)
+            => _translations.TryGetValue(eventType, out var translated)
+                ? TranslationFor(translated)
+                : eventType;
+
+        private string InverseTranslationFor(string eventType)
+            => _inverseTranslations.TryGetValue(eventType, out var translated)
+                ? InverseTranslationFor(translated)
+                : eventType;
 
         public IReadOnlyDictionary<string, Type> Registrations
-            => _registry
+            => _inner
                 .Registrations
                 .ToDictionary(
-                    kvp => _translations.TryGetValue(kvp.Key, out var translated)
-                        ? translated
-                        : kvp.Key,
+                    kvp => TranslationFor(kvp.Key),
                     kvp => kvp.Value);
     }
 }
