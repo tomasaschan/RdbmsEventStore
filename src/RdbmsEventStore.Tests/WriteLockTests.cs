ac019a37c38ce713107d151b62ab11478dc94736
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-25 20:47:36 +0200

    Add missing tests for write lock

---
+++ src/RdbmsEventStore.Tests/WriteLockTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace RdbmsEventStore.Tests
+{
+    public class WriteLockTests
+    {
+        [Fact]
+        public void SameIdCannotEnterLockSimultaneously()
+        {
+            var mutex = new WriteLock<int>();
+
+            var lock1 = mutex.Aquire(1);
+            var lock2 = mutex.Aquire(1);
+
+            Assert.True(lock1.AsTask().IsCompleted);
+            Assert.False(lock2.AsTask().IsCompleted);
+        }
+
+        [Fact]
+        public void DifferentIdsCanEnterLockSimultaneously()
+        {
+            var mutex = new WriteLock<int>();
+
+            var lock1 = mutex.Aquire(1);
+            var lock2 = mutex.Aquire(2);
+
+            Assert.True(lock1.AsTask().IsCompleted, "First stream could not enter locked path.");
+            Assert.True(lock2.AsTask().IsCompleted, "Second stream could not enter locked path.");
+        }
+    }
+}
