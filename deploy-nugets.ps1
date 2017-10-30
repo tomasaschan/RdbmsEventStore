fb3fb0bc12c25b5033741fffdef63dec673379d5
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-30 09:41:10 +0100

    Dont push same release twice (#32)
    * Don't push to nuget.org if a package with same release number is already listed
    
    * Fix vscode warnings about aliases
    
    * Add dist folder to .gitignore
    
    * Add packages dir to build cache

--- deploy-nugets.ps1
+++ deploy-nugets.ps1
@@ -3,7 +3,21 @@ if ((gitversion /showvariable prereleaselabel) -eq "feat") {
     return;
 }
 mkdir dist
-$artifacts.keys | % { $artifacts[$_]['sourcePath'] } | ? { $_ -notlike '*.symbols.nupkg' } | Copy-Item -Destination dist
-$packages = ls dist | select -ExpandProperty FullName | ? { $_ -notlike '*.symbols.nupkg' }
-$packages | % { dotnet nuget push $_ -k $env:NUGET_API_KEY -sk $env:NUGET_API_KEY -s https://nuget.org/api/v2/package -ss https://nuget.smbsrc.net/ }
+
+$artifacts.keys `
+    | ForEach-Object { $artifacts[$_]['sourcePath'] } `
+    | Where-Object { $_ -notlike '*.symbols.nupkg' } `
+    | Copy-Item -Destination dist
+$packages = Get-ChildItem dist `
+    | Select-Object -ExpandProperty FullName `
+    | Where-Object { $_ -notlike '*.symbols.nupkg' }
+
+$pushed = nuget list RdbmsEventStore `
+    | Select-Object @{ Name="FileName"; Expression = { "$($_.Replace(" ", ",")).nupkg" } } `
+    | Select-Object -ExpandProperty FileName
+
+$packages `
+    | Where-Object { ($_ | Split-Path -Leaf) -notin $pushed } `
+    | ForEach-Object { dotnet nuget push $_ -k $env:NUGET_API_KEY -sk $env:NUGET_API_KEY -s https://nuget.org/api/v2/package -ss https://nuget.smbsrc.net/ }
+
 if ($LastExitCode -ne 0) { throw; }
