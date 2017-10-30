29d0b0e10a8d93e949d1c4aa20a0e036a5c69ef6
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-30 09:31:33 +0100

    Don't push to nuget.org if a package with same release number is already listed

--- deploy-nugets.ps1
+++ deploy-nugets.ps1
@@ -5,5 +5,12 @@ if ((gitversion /showvariable prereleaselabel) -eq "feat") {
 mkdir dist
 $artifacts.keys | % { $artifacts[$_]['sourcePath'] } | ? { $_ -notlike '*.symbols.nupkg' } | Copy-Item -Destination dist
 $packages = ls dist | select -ExpandProperty FullName | ? { $_ -notlike '*.symbols.nupkg' }
-$packages | % { dotnet nuget push $_ -k $env:NUGET_API_KEY -sk $env:NUGET_API_KEY -s https://nuget.org/api/v2/package -ss https://nuget.smbsrc.net/ }
+
+$pushed = nuget list RdbmsEventStore `
+    | Select-Object @{ Name="FileName"; Expression = { "$($_.Replace(" ", ",")).nupkg" } } `
+    | Select-Object -ExpandProperty FileName
+
+$packages `
+    | Where-Object { ($_ | Split-Path -Leaf) -notin $pushed } `
+    | % { dotnet nuget push $_ -k $env:NUGET_API_KEY -sk $env:NUGET_API_KEY -s https://nuget.org/api/v2/package -ss https://nuget.smbsrc.net/ }
 if ($LastExitCode -ne 0) { throw; }
