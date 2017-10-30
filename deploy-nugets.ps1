c8116bfb3e68e4d11bb638fcd9aa724e9be97f0f
Tomas Lycken <tlycken@users.noreply.github.com>
2017-10-30 09:31:58 +0100

    Fix vscode warnings about aliases

--- deploy-nugets.ps1
+++ deploy-nugets.ps1
@@ -3,8 +3,14 @@ if ((gitversion /showvariable prereleaselabel) -eq "feat") {
     return;
 }
 mkdir dist
-$artifacts.keys | % { $artifacts[$_]['sourcePath'] } | ? { $_ -notlike '*.symbols.nupkg' } | Copy-Item -Destination dist
-$packages = ls dist | select -ExpandProperty FullName | ? { $_ -notlike '*.symbols.nupkg' }
+
+$artifacts.keys `
+    | ForEach-Object { $artifacts[$_]['sourcePath'] } `
+    | Where-Object { $_ -notlike '*.symbols.nupkg' } `
+    | Copy-Item -Destination dist
+$packages = Get-ChildItem dist `
+    | Select-Object -ExpandProperty FullName `
+    | Where-Object { $_ -notlike '*.symbols.nupkg' }
 
 $pushed = nuget list RdbmsEventStore `
     | Select-Object @{ Name="FileName"; Expression = { "$($_.Replace(" ", ",")).nupkg" } } `
@@ -12,5 +18,6 @@ $pushed = nuget list RdbmsEventStore `
 
 $packages `
     | Where-Object { ($_ | Split-Path -Leaf) -notin $pushed } `
-    | % { dotnet nuget push $_ -k $env:NUGET_API_KEY -sk $env:NUGET_API_KEY -s https://nuget.org/api/v2/package -ss https://nuget.smbsrc.net/ }
+    | ForEach-Object { dotnet nuget push $_ -k $env:NUGET_API_KEY -sk $env:NUGET_API_KEY -s https://nuget.org/api/v2/package -ss https://nuget.smbsrc.net/ }
+
 if ($LastExitCode -ne 0) { throw; }
