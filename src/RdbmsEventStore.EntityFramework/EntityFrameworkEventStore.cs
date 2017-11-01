81238d3fde467d8bce1f5041a3af6978c502cc30
Tomas Lycken <tlycken@users.noreply.github.com>
2017-11-01 10:32:29 +0100

    Simplify event stream (#36)
    * Simplify event store by re-using overloads
    
    * Move QueryableExtensions to System.Linq
    
    * Fix naming of private field
    
    * Implement derivable overloads with extension methods
    
    * Rename IEventStream back to IEventStore

--- src/RdbmsEventStore.EntityFramework/EntityFrameworkEventStore.cs
+++ src/RdbmsEventStore.EntityFramework/EntityFrameworkEventStore.cs
@@ -7,7 +7,7 @@ using RdbmsEventStore.Serialization;
 
 namespace RdbmsEventStore.EntityFramework
 {
-    public class EntityFrameworkEventStore<TId, TStreamId, TContext, TEvent, TEventMetadata, TPersistedEvent> : IEventStream<TStreamId, TEvent, TEventMetadata>
+    public class EntityFrameworkEventStore<TId, TStreamId, TContext, TEvent, TEventMetadata, TPersistedEvent> : IEventStore<TStreamId, TEvent, TEventMetadata>
         where TId : IEquatable<TId>
         where TStreamId : IEquatable<TStreamId>
         where TContext : DbContext, IEventDbContext<TPersistedEvent>
@@ -15,24 +15,22 @@ namespace RdbmsEventStore.EntityFramework
         where TPersistedEvent : class, IPersistedEvent<TStreamId>, TEventMetadata, new()
         where TEventMetadata : class, IEventMetadata<TStreamId>
     {
-        private readonly TContext context;
+        private readonly TContext _context;
         private readonly IEventFactory<TStreamId, TEvent> _eventFactory;
         private readonly IWriteLock<TStreamId> _writeLock;
         private readonly IEventSerializer<TEvent, TPersistedEvent> _serializer;
 
         public EntityFrameworkEventStore(TContext context, IEventFactory<TStreamId, TEvent> eventFactory, IWriteLock<TStreamId> writeLock, IEventSerializer<TEvent, TPersistedEvent> serializer)
         {
-            this.context = context;
+            _context = context;
             _eventFactory = eventFactory;
             _writeLock = writeLock;
             _serializer = serializer;
         }
 
-        public  Task<IEnumerable<TEvent>> Events() => Events(events => events);
-
         public async Task<IEnumerable<TEvent>> Events(Func<IQueryable<TEventMetadata>, IQueryable<TEventMetadata>> query)
         {
-            var storedEvents = await context.Events
+            var storedEvents = await _context.Events
                 .AsNoTracking()
                 .Apply(query)
                 .OrderBy(e => e.Timestamp)
@@ -45,32 +43,11 @@ namespace RdbmsEventStore.EntityFramework
             return events;
         }
 
-        public Task<IEnumerable<TEvent>> Events(TStreamId streamId) => Events(streamId, events => events);
-
-        public async Task<IEnumerable<TEvent>> Events(TStreamId streamId, Func<IQueryable<TEventMetadata>, IQueryable<TEventMetadata>> query)
-        {
-            var storedEvents = await context.Events
-                    .Where(e => e.StreamId.Equals(streamId))
-                    .AsNoTracking()
-                    .Apply(query)
-                    .OrderBy(e => e.Timestamp)
-                    .ToListAsync();
-
-            var events = storedEvents
-                .Cast<TPersistedEvent>()
-                .Select(_serializer.Deserialize);
-
-            return events;
-        }
-
-        public Task Append(TStreamId streamId, long versionBefore, object payload)
-            => Append(streamId, versionBefore, new[] { payload });
-
         public async Task Append(TStreamId streamId, long versionBefore, IEnumerable<object> payloads)
         {
             using (await _writeLock.Aquire(streamId))
             {
-                var highestVersionNumber = await context.Events
+                var highestVersionNumber = await _context.Events
                     .Where(e => e.StreamId.Equals(streamId))
                     .Select(e => e.Version)
                     .DefaultIfEmpty(0)
@@ -82,8 +59,8 @@ namespace RdbmsEventStore.EntityFramework
                 }
 
                 var events = _eventFactory.Create(streamId, versionBefore, payloads).Select(_serializer.Serialize);
-                context.Events.AddRange(events);
-                await context.SaveChangesAsync();
+                _context.Events.AddRange(events);
+                await _context.SaveChangesAsync();
             }
         }
     }
---
+++ src/RdbmsEventStore/QueryableExtensions.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Linq
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Apply<T>(this IQueryable<T> source, Func<IQueryable<T>, IQueryable<T>> projection) => projection(source);
+    }
+}
